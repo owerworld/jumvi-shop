dddbf878f685e9ef205316b9329c2ea011e41a7f
RAMO <ramo@unknown1ec5a9d812dd.home>
2026-02-07 05:41:12 +0300

    fix: remove www redirect to avoid loop

--- next.config.mjs
+++ next.config.mjs
@@ -4,21 +4,6 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.jumvi.co",
-          },
-        ],
-        destination: "https://jumvi.co/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
