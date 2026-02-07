fc03729bd9d97bedb8b3f0a2a8fd72b86389817d
RAMO <ramo@unknown1ec5a9d812dd.home>
2026-02-07 05:22:45 +0300

    chore: redirect www to root

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,21 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.jumvi.co",
+          },
+        ],
+        destination: "https://jumvi.co/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
